7912a3d7ba9cc3c02401fbcc0a3ca6812de3a927
Javier Catalá <javicatala@iesdoctorbalmis.com>
2019-11-08 15:04:33 +0100

    Commit inicial

--- FolderBrowserControl.xaml.cs
+++ FolderBrowserControl.xaml.cs
@@ -20,9 +20,68 @@ namespace FolderBrowser
     /// </summary>
     public partial class FolderBrowserControl : UserControl
     {
+
+        //Propiedades de dependencia
+        public string FolderPath
+        {
+            get { return (string)GetValue(FolderPathProperty); }
+            set { SetValue(FolderPathProperty, value); }
+        }
+
+        public static readonly DependencyProperty FolderPathProperty =
+    DependencyProperty.Register("FolderPath", typeof(string), typeof(FolderBrowserControl), new PropertyMetadata(""));
+
+
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelProperty =
+    DependencyProperty.Register("Label", typeof(string), typeof(FolderBrowserControl), new PropertyMetadata(""));
+
+
+        public bool IsReadOnly
+        {
+            get { return (bool)GetValue(IsReadOnlyProperty); }
+            set { SetValue(IsReadOnlyProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsReadOnlyProperty =
+            DependencyProperty.Register("IsReadOnly", typeof(bool), typeof(FolderBrowserControl), new PropertyMetadata(true));
+
+
+
+        public bool AllowNewFolder
+        {
+            get { return (bool)GetValue(AllowNewFolderProperty); }
+            set { SetValue(AllowNewFolderProperty, value); }
+        }
+
+        public static readonly DependencyProperty AllowNewFolderProperty =
+            DependencyProperty.Register("AllowNewFolder", typeof(bool), typeof(FolderBrowserControl), new PropertyMetadata(true));
+
+
         public FolderBrowserControl()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        private void SeleccionarButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialogo = new System.Windows.Forms.FolderBrowserDialog
+            {
+                ShowNewFolderButton = AllowNewFolder,
+                SelectedPath = FolderPath
+            };
+
+            if (dialogo.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FolderPath = dialogo.SelectedPath;
+            }
+
         }
     }
 }
